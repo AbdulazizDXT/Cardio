f233ae749f9a029d3985756049fc8e58a4a8d130
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-14 21:46:09 +0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,10 +1,7 @@
 #-------------TEST COMMANDS---------------#
 #shutdown /r /t 00
-#taskkill /IM cmd.exe /F
 #-------------AUTO UPDATE Massage.bat SCRIPT---------------#
 del UP.bat
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Massage.bat -OutFile Massage.bat"
 #-------------COMMANDS---------------#
-#taskkill /IM cmd.exe /F
-#---------------GoToStart--------------#
 exit
