62de181bcb9b7cf98596fab5db66e91f6e019f8b
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-15 22:17:34 +0400

    Delete Install.bat

--- Install.bat
+++
@@ -1,8 +0,0 @@
-powershell -command "Invoke-WebRequest https://github.com/AbdulazizDXT/Cardio/archive/refs/heads/main.zip -OutFile Cardio.zip"
-powershell -command "Expand-Archive Cardio.zip"
-cd Cardio
-mkdir C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Cardio
-copy * C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Cardio
-start StartUP.vbs
-cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Cardio
-start Massage.py
