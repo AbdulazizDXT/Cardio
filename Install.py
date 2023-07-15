4496dbc50603e58ec026ff64777392900a6c1be6
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-15 22:17:41 +0400

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,13 @@
+import urllib.request
+import os
+import sys
+os.system("cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft")
+print("#-----------------------------------------------------------------------------------#")
+print("You must fork the repository to begin with and then enter your username here exactly!")
+print("#-----------------------------------------------------------------------------------#")
+USER = input("GitHub USERNAME :")
+urllib.request.urlretrieve(f"https://github.com/{USER}/Cardio/archive/refs/heads/main.zip", "Cardio.zip")
+os.system('copy Cardio.zip C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft')
+os.system('cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft && powershell -command "Expand-Archive Cardio.zip"')
+os.system('cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft\\Cardio\\Cardio-main && start StartUP.vbs && start Massage.py')
+os.system('exit')
