6275ea8704f04c484d93473825627ef0292856ff
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-16 22:49:56 +0400

    Update Update.bat

--- TestFiles/Update.bat
+++ TestFiles/Update.bat
@@ -5,4 +5,4 @@
 del UP.bat                                                               #-----------#
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Message.pyw -OutFile Message.pyw"
 #-------------COMMANDS (Add Commands here!)---------------#
-msg * Come on guys, it's time for lunch, and then let's meet in the conference room.
+msg * Ok Check!!!
