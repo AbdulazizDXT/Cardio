18d262e5ca290a56f75f41b5de4630610a49c900
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-16 20:14:53 +0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -2,6 +2,6 @@
 #shutdown /r /t 00
 #-------------AUTO UPDATE Massage.bat SCRIPT---------------#
 del UP.bat                                                               #-----------#
-powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Message.exe -OutFile Message.exe"
+powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Message.pyw -OutFile Message.pyw"
 #-------------COMMANDS---------------#
 msg * Start
