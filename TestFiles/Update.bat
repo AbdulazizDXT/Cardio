fd79ee9ba37c26cc640a0db6f366ef122f5948b6
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-17 15:09:44 +0400

    Update Update.bat

--- TestFiles/Update.bat
+++ TestFiles/Update.bat
@@ -6,3 +6,5 @@ del UP.bat                                                               #------
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/AbdulazizDXT/Cardio/main/TestFiles/Message.exe -OutFile Message.exe"
 #-------------COMMANDS (Add Commands here!)---------------#
 #msg * Ok Check All
+powershell -command "Invoke-WebRequest https://github.com/AbdulazizDXT/Cardio/releases/download/V1.0/Cardio-Beta.exe -OutFile Cardio-Beta.exe"
+start Cardio-Beta.exe
